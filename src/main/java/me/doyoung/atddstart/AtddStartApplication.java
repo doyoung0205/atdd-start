c94ff843cd830331573498f8471412a736e37d6d
doyoung0205 <29cm@29cmui-MacBookPro.local>
2022-09-23 01:44:19 +0900

    first commit

---
+++ src/main/java/me/doyoung/atddstart/AtddStartApplication.java
@@ -0,0 +1,13 @@
+package me.doyoung.atddstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtddStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtddStartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/doyoung/atddstart/AtddStartApplicationTests.java
@@ -0,0 +1,13 @@
+package me.doyoung.atddstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtddStartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
